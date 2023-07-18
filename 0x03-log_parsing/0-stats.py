fa08ad75b791510cd374d3f3154aaf64ad9faacf
lekseel <olalekanshogbaike@gmail.com>
2023-07-18 01:55:45 -0700

    parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,45 +1,45 @@
 #!/usr/bin/python3
-
-"""Script that reads stdin line by line and computes metrics"""
+"""
+Script that reads stdin line by line and computes metrics:
+"""
 
 import sys
 
 
-def printsts(dic, size):
-    """ WWPrints information """
-    print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+if __name__ == "__main__":
 
+    status_code = {"200": 0, "301": 0, "400": 0, "401": 0,
+                   "403": 0, "404": 0, "405": 0, "500": 0}
+    file_size = 0
+    total_lines = 0
 
-sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-       "404": 0, "405": 0, "500": 0}
+    def print_values(status_code, file_size):
+        print("File size: {:d}".format(file_size))
+        for key in sorted(status_code.keys()):
+            if status_code[key] != 0:
+                print("{}: {:d}".format(key, status_code[key]))
 
-count = 0
-size = 0
+    try:
+        for line in sys.stdin:
+            if total_lines != 0 and total_lines % 10 == 0:
+                print_values(status_code, file_size)
 
-try:
-    for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printsts(sts, size)
+            total_lines += 1
+            ln = line.split()
 
-        stlist = line.split()
-        count += 1
+            try:
+                file_size += int(ln[-1])
+            except:
+                pass
 
-        try:
-            size += int(stlist[-1])
-        except:
-            pass
+            try:
+                if ln[-2] in status_code:
+                    status_code[ln[-2]] += 1
+            except:
+                pass
 
-        try:
-            if stlist[-2] in sts:
-                sts[stlist[-2]] += 1
-        except:
-            pass
-    printsts(sts, size)
+        print_values(status_code, file_size)
 
-
-except KeyboardInterrupt:
-    printsts(sts, size)
-    raise
+    except KeyboardInterrupt:
+        print_values(status_code, file_size)
+        raise
